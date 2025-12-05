be7ed34f1d4867427cc25511f46d7cb302c3feba
zaki-moh <zakgarad218@gmail.com>
2025-12-05 00:31:58 -0600

    Header done

--- sports-predictor-frontend/components/ui/Header.tsx
+++ sports-predictor-frontend/components/ui/Header.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
@@ -6,10 +7,10 @@ import NavItems from './NavItems'
 const Header = () => {
   return (
     <header className="sticky top-0 bg-[#050D27] shadow-md z-50">
-      <div className="flex items-center w-full justify-between pl-8 pr-20">
-        <Link href="/"> 
+      <div className="flex items-center w-full justify-between pl-8 pr-4">
+        <Link href="/" className="cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 active:scale-90"> 
           <Image 
-            src="/assets/icons/sportaAI.logo.png"
+            src="/assets/icons/finalLogo.png"
             alt="logo" 
             width={1526} 
             height={1024} 
--- sports-predictor-frontend/components/ui/NavItems.tsx
+++ sports-predictor-frontend/components/ui/NavItems.tsx
@@ -4,51 +4,51 @@ import Image from 'next/image'
 
 const NavItems = () => {
   return (
-    <ul className="flex flex-row gap-10 py-2 font-bold">
-      <li>
+    <ul className="flex flex-row gap-4 font-bold">
+      <li className="w-26 flex justify-center cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 active:scale-90">
         <Link href="#">
           <Image
             src="/assets/icons/UFC.webp"
             alt="UFC"
             width={1024}
             height={379}
-            className="h-10 w-auto cursor-pointer"
+            className="h-10 w-auto cursor-pointer scale-60"
           />
         </Link>
       </li>
 
-      <li>
+      <li className="w-16 flex justify-center cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 active:scale-90">
         <Link href="#">
           <Image
             src="/assets/icons/NBA.png"
             alt="NBA"
             width={1024}
             height={379}
-            className="h-10 w-auto cursor-pointer"
+            className="h-10 w-auto cursor-pointer sclae-110"
           />
         </Link>
       </li>
 
-      <li>
+      <li className="w-16 flex justify-center cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 active:scale-90">
         <Link href="#">
           <Image
             src="/assets/icons/NFLlogo.png"
             alt="NFL"
             width={1024}
             height={379}
-            className="h-10 w-auto cursor-pointer"
+            className="h-10 w-auto cursor-pointer sclae-110"
           />
         </Link>
       </li>
 
-      <li>
+      <li className="w-16 flex justify-center cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 active:scale-90">
         <Link href="#">
           <Image
             src="/assets/icons/PremLeugue.png"
             alt="Premier League"
             width={1024}
             height={379}
-            className="h-10 w-auto cursor-pointer"
+            className="h-10 w-auto cursor-pointer scale-110"
           />
         </Link>
       </li>
