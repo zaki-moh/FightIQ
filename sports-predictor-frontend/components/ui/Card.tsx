dbf531a218193bbe8e1f3b52b400d1e055a98d07
zaki-moh <zakgarad218@gmail.com>
2025-12-11 20:22:13 -0600

    landing page done

---
+++ sports-predictor-frontend/components/ui/Card.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+interface CardProps {
+    icon: React.ReactNode;
+    title: string
+    description: string;
+}
+
+const Card = ({
+    icon,
+    title, 
+    description
+}: CardProps) => {
+  return (
+    <div className="bg-white/5 backdrop-blur-sm rounded-xl p-8 border -border-white/10 hover:border-white/20 transition-all mt-10 w-80">
+        <div className="mb-4">{icon}</div>
+        <h3 className="text-white text-xl font-bold mb-2">{title}</h3>
+        <p className="text-white/60">{description}</p>
+    </div>
+  )
+}
+
+export default Card
--- sports-predictor-frontend/components/ui/Welcome.tsx
+++ sports-predictor-frontend/components/ui/Welcome.tsx
@@ -3,7 +3,8 @@
 import React from 'react'
 import Button from './Button'
 import { useRouter } from "next/navigation"
-import { BoltIcon } from "@heroicons/react/24/solid";
+import { BoltIcon, ChartBarIcon, GlobeAmericasIcon, SparklesIcon} from "@heroicons/react/24/solid";
+import Card from './Card';
 
 
 const Welcome = () => {
@@ -13,11 +14,11 @@ const Welcome = () => {
   }
   return (
     <div className="flex flex-col items-center justify-center">
-      <section className="flex flex-col w-full items-center justify-center gap-6 mt-32 opacity-0 animate-fade-in delay-0">
+      <section className="flex flex-col w-full items-center justify-center gap-6 mt-24 opacity-0 fade-in-up animate-[delay-150ms]">
         <h1 className="text-5xl font-semibold text-white text-center">
           Welcome to Sporta AI!
         </h1>
-        <h2 className="text-xl font-light text-gray-500 text-center animate-fade-in delay-150">AI-powered sports predictions and analytics</h2>
+        <h2 className="text-xl font-light text-gray-500 text-center fade-in-up animate-[delay-150ms]">AI-powered sports predictions and analytics</h2>
         <div className="flex gap-8">
           <Button variant="primary" size='lg' onClick={onClick} >
             Get started
@@ -28,12 +29,20 @@ const Welcome = () => {
         </div>
       </section>
 
-      <div className="w-3/4 mx-auto border-t-2 max-w-4xl bg-white/30 h-[3px] mt-10
-        bg-gradient-to-r from-transparent via-white/20 to-transparent"/>
-      <div className="flex items-center justify-center gap-6 mt-10">
+      <div   className="
+                w-3/4 
+                max-w-4xl 
+                mx-auto 
+                mt-10 
+                h-[2px]
+                bg-gradient-to-r 
+                from-transparent 
+                via-white/20 
+                to-transparent"/>
+      <div className="flex items-center justify-center gap-4 mt-6">
         <BoltIcon className="w-8 h-8 text-white" />
         
-        <p className="text-white/30 text-3xl">|</p>
+        <p className="text-white/20 text-3xl">|</p>
 
         <h3 className="font-semibold text-white text-2xl">
           Real-time ML predictions
@@ -44,6 +53,11 @@ const Welcome = () => {
         <h3 className="font-semibold text-white text-2xl">
           70% accuracy
         </h3>
+      </div>
+      <div className="flex gap-4 mt-12 fade-in-up animate-[delay-300ms]">
+        <Card icon={<ChartBarIcon className="w-10 h-10 text-purple-400" />} title="Data-Driven Insights" description="Our machine learning pipeline continuously improves accuracy."/>
+        <Card icon={<SparklesIcon className="w-10 h-10 text-yellow-400" />} title="Smart ML Engine" description="Our machine learning pipeline continuously improves accuracy."/>
+        <Card icon={<GlobeAmericasIcon className="w-10 h-10 text-cyan-400" />} title="Multi-Sport Support" description="SportaAI adapts its prediction engine to UFC, NBA, NFL, Premier League, and more — tailored to each sport’s unique analytics."/>
       </div>
     </div>
   )
