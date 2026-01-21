886d28545efb7ac8395bfcbed06972e82aa97b68
Zakaria Mohamed <zakariamohamed@Zakarias-MacBook-Pro.local>
2026-01-20 19:09:38 -0600

    test

--- backend/src/main.py
+++ backend/src/main.py
@@ -14,7 +14,7 @@ origins = os.getenv(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
