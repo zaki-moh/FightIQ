182046ac68522dbe5264d1d7d89c7e9066a041d2
zaki-moh <zakgarad218@gmail.com>
2026-02-12 16:22:09 -0600

    typo fix

--- frontend/components/ui/PerformanceMetric.tsx
+++ frontend/components/ui/PerformanceMetric.tsx
@@ -29,7 +29,7 @@ const PerformanceMetric = (props: PerformanceMetricProps) => {
                 Career Stage
             </span>
             <span className={`px-3 py-1 rounded-full text-xs font-semibold ${stageStyles[props.value] || 'bg-white/10 text-white/50 border-white/20'}`}>
-                Veteran
+                {props.value}
             </span>
         </div>
     )
